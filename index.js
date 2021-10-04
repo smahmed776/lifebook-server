0af66b8b4af97c679bf44f3b9bd1576a26282ad9
Syed Mahbub <xsmahmed776@gmail.com>
2021-10-04 17:13:22 +0600

    org

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(PORT, ()=> {
 
 if(process.env.NODE_ENV == 'production'){
     app.use(cors({
-    origin: "*",
+    origin: "https://smahmed776.github.io/lifebook",
         credentials: true,
     }))
 } else {
